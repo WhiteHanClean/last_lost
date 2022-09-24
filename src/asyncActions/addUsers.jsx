494fb24130563cca0e7fb3bd03155800b986a9f7
beku026 <105550510+beku026@users.noreply.github.com>
2022-09-25 00:43:22 +0600

    Delete src directory

--- src/asyncActions/addUsers.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import { AddUser } from '../reducer/AddUserReducer'
-
-export const RequestUsers = () => {
-    return function(dispatch) {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(response => dispatch(AddUser(response)))
-    }
-}
